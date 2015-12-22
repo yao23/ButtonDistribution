f4ac139a79f59204661480facc4a9c92fe9e56c2
Yao <myspiritcrazy@gmail.com>
2015-12-22 10:12:55 -0800

    add constraints

--- ButtonDistribution/ViewController.swift
+++ ButtonDistribution/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shopBtn: UIButton!
+    @IBOutlet weak var likeBtn: UIButton!
+    @IBOutlet weak var commentBtn: UIButton!
+    @IBOutlet weak var shareBtn: UIButton!
+    
+    @IBOutlet weak var firstViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var secondViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var thirdViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var fourthViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var fifthViewWidthConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +32,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func shopAction(sender: AnyObject) {
+    }
+    @IBAction func likeAction(sender: AnyObject) {
+    }
+    @IBAction func commentAction(sender: AnyObject) {
+    }
 }
 
